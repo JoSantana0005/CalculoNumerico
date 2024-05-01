fb1a24c3b97c66857ffae9d10238c5ff98836c32
JoSantana0005 <joseasantana05@gmail.com>
2024-04-30 23:19:25 -0400

    Se elimino la carepta

--- EjerciciosenClase/EjerciciosenR.R
+++
@@ -1,9 +0,0 @@
-#Programa que realiza el Teorema de Pitagoras
-
-catetoa <- as.numeric(readline("Introduzca el cateto a: "))
-catetob <- as.numeric(readline("Introduzca el cateto b: "))
-catetoa
-catetob
-hipo = sqrt((catetoa)**2 + (catetob)**2)
-hipo
-print("La hipotenusa es de: " + hipo)
