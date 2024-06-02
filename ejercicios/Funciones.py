854ff573febe5db5b571efe7dd26276c5688885d
JoSantana0005 <joseasantana05@gmail.com>
2024-06-02 01:08:13 -0400

    Nueva funcion que muestra la norma matricial

--- ejercicios/Funciones.py
+++ ejercicios/Funciones.py
@@ -8,7 +8,14 @@ def NormadeVector(x1,x2):
     norma = np.linalg.norm(x2-x1)
     print(norma)
     plt.plot([0,norma], label= "Norma")
+    plt.title("Norma del vector: {:3f}".format(norma))
     plt.legend()
     plt.show()
 
+def NormaMatricial(x1):
+    norma = np.linalg.norm(x1)
+    plt.plot([0,norma], label="Norma")
+    plt.title("Norma de la matriz:  {:3f}".format(norma))
+    plt.legend()
+    plt.show()
 
